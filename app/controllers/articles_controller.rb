663e08ea65d3a40bff6b145b9aee4d07c4451988
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2019-05-12 13:41:51 -0300

    create users table and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+    def index 
+        @articles = Article.all
+    end 
 
     def new 
         @article = Article.new
@@ -18,7 +23,28 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was update"
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = "Article was not updated"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy 
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path 
+    end
+
     private
+        def set_article 
+            @article = Article.find(params[:id])
+        end
         def article_params
             params.require(:article).permit(:title, :description)
         end
